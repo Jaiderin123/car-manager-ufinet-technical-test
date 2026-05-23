73a0930cc2eabe4832a4817d5bd941c95be32f5f
Jaiderin123 <torrezjaider10@gmail.com>
2026-05-22 22:30:11 -0500

    feat: initial repository structure and backend base setup

---
+++ car-manager-backend/src/test/java/com/ufinet/carmanager/CarManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufinet.carmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
